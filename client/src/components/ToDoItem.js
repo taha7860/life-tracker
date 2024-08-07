a18e02e895e63cf42494a0db279199f349648c1e
Taha Ejaz <tahaejaz22@icloud.com>
2024-08-07 02:19:52 +0100

    Added edit and delete buttons for to-do list components

--- client/src/components/ToDoItem.js
+++ client/src/components/ToDoItem.js
@@ -1,6 +1,12 @@
 import '../styles/ToDoItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 function ToDoItem(props) {
+
+
     return (
         <li className="todo-item">
             <div className="checkbox-container">
@@ -9,7 +15,11 @@ function ToDoItem(props) {
             </div>
             <div className="todo-container">
                 <section>{props.name}</section>
-                <section>{props.startTime} - {props.endTime}</section>
+                <section><FontAwesomeIcon icon={faClock} className="clock"></FontAwesomeIcon> {props.startTime} - {props.endTime}</section>
+            </div>
+            <div className="edit-container">
+                <FontAwesomeIcon icon={faEdit} className="edit"></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faTrashAlt} className="delete"></FontAwesomeIcon>
             </div>
         </li>
     );
